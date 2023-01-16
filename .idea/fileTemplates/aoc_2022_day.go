e5b2584f0401612d974ffec763b884c1e81e727a
eoghan <eoghancurtin93@gmail.com>
2023-01-16 15:28:00 +0800

    add goland file template

---
+++ .idea/fileTemplates/aoc_2022_day.go
@@ -0,0 +1,23 @@
+package day${AOC_DAY}
+
+import (
+	"aoc2022/days"
+	"aoc2022/utils"
+	"strconv"
+)
+
+var input = utils.ReadInputAsStrings(${AOC_DAY})
+var Solution = days.Day{
+	Part1: part1,
+	Part2: part2,
+}
+
+func part1() string {
+
+	return strconv.Itoa(1)
+}
+
+func part2() string {
+
+	return strconv.Itoa(1)
+}
